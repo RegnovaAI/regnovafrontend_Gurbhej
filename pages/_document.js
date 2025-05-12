856daa9846152021680c24e3f136ed3f3e0abdf3
founder@regnovaAI.com <founder@regnovaAI.com>
2025-05-12 21:50:09 +0100

    Product V1

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,47 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body className="antialiased">
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
+
+
+
+
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-3C3TE5ZSQN"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-3C3TE5ZSQN');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
